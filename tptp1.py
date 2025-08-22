06aa17e0c90eec601fea81e35750c08498b792aa
yooboi4925-prog <yooboi.4925@gmail.com>
2025-08-22 18:19:52 +0530

    Add files via upload

---
+++ tptp1.py
@@ -0,0 +1,27 @@
+## task 1
+
+
+a=input('enter first number ')
+a=float(a)
+b=input('enter second number')
+b=float(b)
+
+print (a + b )
+print (a - b )
+print (a*b)
+print (a/b)
+
+### task 2
+
+c=input('what is your first name? ')
+d=input('what is your last name? ')
+
+print ("hello, "+ c + "" +d + "! welcome to python program ")
+
+
+
+
+
+
+
+
